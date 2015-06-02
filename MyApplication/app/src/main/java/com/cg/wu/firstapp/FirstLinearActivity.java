bf287b12ac16051c25f04b83e3d527d8e50fe533
gg <gg.wcg@qq.com>
2015-06-02 11:34:58 +0800

    LinearLayout

--- MyApplication/app/src/main/java/com/cg/wu/firstapp/FirstLinearActivity.java
+++ MyApplication/app/src/main/java/com/cg/wu/firstapp/FirstLinearActivity.java
@@ -2,39 +2,22 @@ package com.cg.wu.firstapp;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Date;
 
 
-public class MainActivity extends ActionBarActivity {
-    private TextView tvHello;
-    private Button btHello;
+public class FirstLinearActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.v("Test", "onCreate event");
-        tvHello = (TextView)findViewById(R.id.tvHello);
-        btHello = (Button)findViewById(R.id.btHello);
-        btHello.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tvHello.setText(new Date().toString());
-            }
-        });
+        setContentView(R.layout.activity_first_linear);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_first_linear, menu);
         return true;
     }
 
